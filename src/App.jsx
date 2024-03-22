34645224234b1d935662d94d051f20bcb5afc53c
brian <yatunyi.15075@students.kyu.ac.ke>
2024-03-22 15:07:28 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { ChatEngine } from 'react-chat-engine';
+// import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
